7e78b315a2bfa1a807187ee4eeb093f93de58a5e
mredyk <edruisl99@gmail.com>
2023-01-05 18:56:54 +0100

    Add first version of data fetching (slow)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import json
 from services import SpeedrunService
 
 
 def main():
     speedrun_service = SpeedrunService()
-    game = speedrun_service.get_game()
-    print(game)
+    statistics = speedrun_service.get_statistics()
+    with open("game_data.json", "w") as f:
+        json.dump(statistics, f, indent=2)
 
 
 if __name__ == "__main__":
--- src/services/speedrun_service.py
+++ src/services/speedrun_service.py
@@ -1,10 +1,82 @@
+from collections import defaultdict
+from time import sleep
+
 import srcomapi
 import srcomapi.datatypes as dt
 
 
-class SpeedrunService:
+class PaginationMixin:
+    max_results = 200
+
+    def _get_data(self, data_type, params=None):
+        raise NotImplementedError
+
+    def _get_all_data(self, data_type, params=None):
+        params = params or {}
+        current_results = 200
+        pagination_params = {
+            "max": self.max_results,
+            "offset": 0
+        }
+
+        while current_results == self.max_results:
+            data = self._get_data(data_type, {**pagination_params, **params})
+            for item in data:
+                yield item
+            
+            current_results = len(data)
+            print(f"Got {pagination_params['offset'] + current_results} results of {data_type.__name__} so far...")
+            pagination_params["offset"] += self.max_results
+
+
+class SpeedrunService(PaginationMixin):
     def __init__(self):
         self.api = srcomapi.SpeedrunCom()
+        self._region_mapping = {}
+    
+    def _get_data(self, data_type, params=None):
+        sleep(0.5)
+        return self.api.search(data_type, params or {})
+    
+    def get_region_data_id_or_name(self, identifier):
+        if identifier is None:
+            return None
+        if not self._region_mapping:
+            for region in self._get_data(dt.Region):
+                self._region_mapping[region.id] = region.name
+                self._region_mapping[region.name] = region.id
+        return self._region_mapping[identifier]
 
-    def get_game(self):
-        return self.api.search(dt.Game, {"name": "super mario sunshine"})
+    def get_games(self):
+        game_params = {
+            "region": self.get_region_data_id_or_name("JPN / NTSC"),
+        }
+        for game in self._get_all_data(dt.Game, game_params):
+            if len(game.regions) >= 2:
+                yield game
+
+    def get_game_runs(self, game_id):
+        all_runs = self._get_all_data(dt.Run, {"game": game_id, "status": "verified"})
+        runs_by_user = defaultdict(list)
+        for run in all_runs:
+            if not run.players:
+                continue
+
+            player = run.players[0]
+            runs_by_user[player.name].append(run)
+
+        for _, runs in runs_by_user.items():
+            runs.sort(key=lambda run: run.times["primary_t"])
+            yield runs[0]
+
+    def get_statistics(self):
+        return [{
+            "id": game.id,
+            "name": game.names["international"],
+            "runs": [{
+                "id": run.id,
+                "player": (player := run.players[0]).name,
+                "player_location": player.location["country"]["code"] if hasattr(player, "location") and player.location else None,
+                "region": self.get_region_data_id_or_name(run.system["region"]),
+            } for run in self.get_game_runs(game.id)],
+        } for game in self.get_games()]
